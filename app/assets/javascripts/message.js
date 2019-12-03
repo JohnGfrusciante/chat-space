4484224a940580d3fb5c35b91d8a06f2e113c62d
JohnGfrusciante <g.rejime.frusciante@gmail.com>
2019-12-03 13:05:18 +0900

    ChatSpace投稿機能の非同期実装

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,57 @@
+$(function(){
+
+  function buildHTML(message){
+
+    if (message.image) {
+      var html = `<div class="mainChat__contents__userName">
+                    ${message.user}
+                    <div class="mainChat__contents__userName__timeStamp">
+                      ${message.created_at}
+                    </div>
+                  </div>
+                  <div class="mainChat__contents__message">
+                    <p>
+                      ${message.content}
+                    </p>
+                      <img class="" src="${message.image}" alt="Mvc">
+                  </div>`
+    } else {
+      var html = `<div class="mainChat__contents__userName">
+                    ${message.user}
+                    <div class="mainChat__contents__userName__timeStamp">
+                      ${message.created_at}
+                    </div>
+                  </div>
+                  <div class="mainChat__contents__message">
+                    <p>
+                      ${message.content}
+                    </p>
+                  </div>`
+    }
+    return html
+  }
+
+  $('#new_message').on('submit', function(e){
+    e.preventDefault()
+    var fromData = new FormData(this);
+    var url = $(this).attr('action');
+    $.ajax({
+      url: url,
+      type: 'POST',
+      data: fromData,  
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(message){
+      var html = buildHTML(message);
+      $('.mainChat__contents').append(html)
+      $('.mainChat__contents').animate({ scrollTop: $('.mainChat__contents')[0].scrollHeight});
+      $('#new_message')[0].reset();
+      $('.sendButton').prop('disabled', false);
+    })
+    .fail(function(){
+      alert("メッセージ送信に失敗しました");
+    })
+  })
+})
